0c73d72ee4f17984eaca252ee809209ba0f67b32
afzal <mafzaldev@gmail.com>
2025-10-16 21:29:42 +0500

    replace mock api with real api

--- src/api/mockApi.ts
+++ src/api/mockApi.ts
@@ -1,24 +1,16 @@
 import type { Event, Registration } from "@/lib/interfaces";
+import axios from "axios";
 
-const mockEvents: Event[] = [
-  {
-    id: "1",
-    name: "React Conference",
-    date: "2025-11-01",
-    location: "New York",
-  },
-  {
-    id: "2",
-    name: "TypeScript Summit",
-    date: "2025-12-05",
-    location: "San Francisco",
-  },
-  { id: "3", name: "Frontend Fest", date: "2026-01-20", location: "Chicago" },
-];
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchEvents = async (): Promise<Event[]> => {
-  await new Promise((res) => setTimeout(res, 1000));
-  return mockEvents;
+  try {
+    const response = await axios.get(`${backendUrl}/events`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    throw new Error("Failed to fetch events");
+  }
 };
 
 export const registerForEvent = async (
